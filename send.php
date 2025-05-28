c163dcabe00c2dd221cf38fba2e658468706249c
EVANS <kiprotichluka70@gmail.com>
2025-05-28 18:51:02 +0300

    Fix: Corrected autoload.php path in send.php

--- send.php
+++ send.php
@@ -3,7 +3,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 // Use Composer's autoloader
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Capture submitted email and password
